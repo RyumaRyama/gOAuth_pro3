ea9514489174852524b2d81e9b62ca22a58f6a15
Daiki Gushiken <e165742@ie.u-ryukyu.ac.jp>
2019-04-15 14:21:18 +0900

    add html

--- web/main.go
+++ web/main.go
@@ -3,13 +3,57 @@ package main
 import (
   "net/http"
   "github.com/labstack/echo"
+  "html/template"
+  "io"
 )
-
-func main() {
-  e := echo.New()
-  e.GET("/login", func(c echo.Context) error {
-    return c.String(http.StatusOK, "hello")
-  })
-  e.Logger.Fatal(e.Start(":1124"))
+type Template struct {
+    templates *template.Template
+}
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+    return t.templates.ExecuteTemplate(w, name, data)
+}
+type ServiceInfo struct {
+  Title string
 }
 
+var serviceInfo = ServiceInfo {
+  "We are goAuth_pro3",
+}
+
+
+func main(){
+
+  t := &Template{
+    templates: template.Must(template.ParseGlob("views/*.html")),
+  }
+
+  e := echo.New()
+
+  e.Renderer = t
+
+  e.Static("/public", "./public/")
+
+  e.GET("/hello", func(c echo.Context) error {
+    return c.String(http.StatusOK, "こんにちは")
+  })
+
+  e.GET("/login", func(c echo.Context) error {
+
+    data := struct {
+      ServiceInfo
+      Content_a string
+      Content_b string
+      Content_c string
+      Content_d string
+    } {
+      ServiceInfo: serviceInfo,
+      Content_a: "goを使って",
+      Content_b: "oauth認証をしよう。",
+      Content_c: "アイウエオ",
+      Content_d: "かきくけこ",
+    }
+    return c.Render(http.StatusOK, "sample", data)
+  })
+
+  e.Logger.Fatal(e.Start(":1124"))
+  }
